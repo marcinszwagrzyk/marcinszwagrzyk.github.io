d594f70a20193e7c26b0dabe31be6b351dcaa954
marcinszwagrzyk <marcin.szwagrzyk@gmail.com>
2024-07-25 08:58:20 +0200

    centroids

--- data/sedona.sql
+++ data/sedona.sql
@@ -1,5 +1,14 @@
+# intersect
 spark.sql("""
 SELECT p.*, pl.id_poligonu, pl.nazwa_poligonu
 FROM punkty p, poligony pl
 WHERE ST_Intersects(p.geometria, pl.geometria)
 """).show()
+
+# centroids
+val result = spark.sql("""
+SELECT ST_Centroid(geometry) AS centroid_geometry
+FROM polygons
+""")
+result.show()
+
