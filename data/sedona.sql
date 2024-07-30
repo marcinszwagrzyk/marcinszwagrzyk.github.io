745ee0b12bd525225220aa054041379f9d51d937
marcinszwagrzyk <marcin.szwagrzyk@gmail.com>
2024-07-30 18:15:33 +0200

    sedona uplaod

--- data/sedona.sql
+++ data/sedona.sql
@@ -18,3 +18,15 @@ SELECT ST_Transform(geometry, 'EPSG:4326', 'EPSG:3035') AS transformed_geometry
 FROM spatial_data
 """)
 transformed.show()
+
+
+-- Wykorzystaj SQL do stworzenia tabeli z geometrią
+spark.sql("""
+    CREATE OR REPLACE TEMP VIEW sedona_points_with_geom AS
+    SELECT 
+        id, 
+        latitude, 
+        longitude, 
+        ST_Point(CAST(longitude AS Decimal(24, 20)), CAST(latitude AS Decimal(24, 20))) AS geom
+    FROM sedona_points
+""")
