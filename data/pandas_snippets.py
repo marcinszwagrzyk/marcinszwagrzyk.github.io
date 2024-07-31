fbdddafd58f4c21af44ab95b7a2f29010d908016
marcinszwagrzyk <marcin.szwagrzyk@gmail.com>
2024-07-31 12:41:40 +0200

    yoy

--- data/pandas_snippets.py
+++ data/pandas_snippets.py
@@ -1,16 +1,8 @@
-# Ustaw indeks na date, state i company, aby ułatwić join
-df.set_index(['date', 'state', 'company'], inplace=True)
+# Połącz na podstawie daty poprzedniego roku, stanu i firmy
+df_merged = df.merge(df, left_on=['prev_year_date', 'state', 'company'], right_on=['date', 'state', 'company'], suffixes=('', '_prev'))
 
-# Kopiuj DataFrame i przesuń daty o rok do przodu
-df_prev_year = df.copy()
-df_prev_year.index = df_prev_year.index.set_levels(df_prev_year.index.get_level_values('date') + pd.DateOffset(years=1), level='date')
+# Oblicz metrykę YoY
+df_merged['YoY'] = (df_merged['value'] / df_merged['value_prev']) - 1
 
-# Połącz oryginalny DataFrame z przesuniętym o rok
-df_yoy = df.join(df_prev_year, rsuffix='_prev')
-
-# Oblicz YoY
-df_yoy['YoY'] = (df_yoy['value'] / df_yoy['value_prev']) - 1
-df_yoy.reset_index(inplace=True)
-
-# Opcjonalnie, odfiltruj tylko potrzebne kolumny
-df_yoy = df_yoy[['date', 'state', 'company', 'YoY']]
+# Wyświetl wyniki
+print(df_merged[['date_x', 'state', 'company', 'value', 'value_prev', 'YoY']])
