4290a5eaae745b97904c55c57da4a802bba9258b
marcinszwagrzyk <marcin.szwagrzyk@gmail.com>
2024-08-01 18:09:33 +0200

    melt

--- data/pandas_snippets.py
+++ data/pandas_snippets.py
@@ -9,3 +9,10 @@ df_merged['YoY'] = (df_merged['value'] / df_merged['value_prev']) - 1
 
 # Wyświetl wyniki
 print(df_merged[['date_x', 'state', 'company', 'value', 'value_prev', 'YoY']])
+
+
+
+# Zachowujemy kolumny 'a', 'b', 'c' i "topimy" kolumny 'x', 'y', 'z'
+df_melted = df.melt(id_vars=['a', 'b', 'c'], value_vars=['x', 'y', 'z'], var_name='column_name', value_name='value')
+
+print(df_melted)
